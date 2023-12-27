b5b5152db7bb3dcd1b36d1df0a316d830208284c
apple <apple@appleui-iMac.skbroadband>
2023-12-27 11:16:59 +0900

    231227 version 0.0.2

--- custom.js
+++ custom.js
@@ -9,8 +9,26 @@ const img = document.querySelector("img");
 
 // console.log(b);
 
+/*
 const a = 10;
 const b = a.toString().padStart(4, "0"); // 내가 채운 숫자 이외에는 0으로 처리하는 기능
 // 지정된 숫자에 대해 첫번째 파라미터 길이로 두번째 파라미터로 문자로 채워줌
 
 console.log(b);
+*/
+
+// Hero Section 높이 지정 : 헤더 높이를 제외한 높이
+const headerHeight = document.querySelector("header").offsetHeight;
+const heroSection = document.querySelector(".section-01");
+
+heroSection.style.marginTop = headerHeight + 50 + "px";
+
+// GSAP 효과 지정
+const imgObj = document.querySelector("#hero-lightpass");
+const mainText = document.querySelector(".main-elem");
+
+let tl0 = gsap.timeline();
+tl0
+  .add("start0") // 시작 지점 지정 (직접 생성하는 이름)
+  .fromTo(imgObj, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0")
+  .fromTo(mainText, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0");
